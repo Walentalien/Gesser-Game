840ad8315a0d79c0f7715bb63191ab0bbcb4b5e1
M. Meier <mi9uel9@gmail.com>
2024-10-24 10:24:21 -0400

    Cleaning up (#3)

--- target/debug/deps/guessing_game-88c48be6983e8d2c.d
+++
@@ -1,5 +0,0 @@
-/home/walentyn/projects/guessing_game/target/debug/deps/guessing_game-88c48be6983e8d2c: src/main.rs
-
-/home/walentyn/projects/guessing_game/target/debug/deps/guessing_game-88c48be6983e8d2c.d: src/main.rs
-
-src/main.rs:
